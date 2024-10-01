0172cf60ef222d751f2e9b913c50377d38291709
LuconhaOliveira <lucaas.oliveira0254@gmail.com>
2024-10-01 09:50:45 -0300

    deixei mais bonito e talvez consertei o background

--- frmMedidas.Designer.cs
+++ frmMedidas.Designer.cs
@@ -59,9 +59,10 @@
             // 
             // btnConvert
             // 
-            btnConvert.Location = new Point(121, 446);
+            btnConvert.Font = new Font("Comic Sans MS", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnConvert.Location = new Point(108, 446);
             btnConvert.Name = "btnConvert";
-            btnConvert.Size = new Size(75, 23);
+            btnConvert.Size = new Size(110, 33);
             btnConvert.TabIndex = 0;
             btnConvert.Text = "Converter";
             btnConvert.UseVisualStyleBackColor = true;
@@ -69,211 +70,237 @@
             // 
             // txtCtoF
             // 
+            txtCtoF.Font = new Font("Comic Sans MS", 9F);
             txtCtoF.Location = new Point(12, 63);
             txtCtoF.Name = "txtCtoF";
-            txtCtoF.Size = new Size(100, 23);
+            txtCtoF.Size = new Size(100, 24);
             txtCtoF.TabIndex = 1;
             // 
             // txtFtoC
             // 
+            txtFtoC.Font = new Font("Comic Sans MS", 9F);
             txtFtoC.Location = new Point(222, 63);
             txtFtoC.Name = "txtFtoC";
-            txtFtoC.Size = new Size(100, 23);
+            txtFtoC.Size = new Size(100, 24);
             txtFtoC.TabIndex = 2;
             // 
             // txtCtoK
             // 
+            txtCtoK.Font = new Font("Comic Sans MS", 9F);
             txtCtoK.Location = new Point(12, 109);
             txtCtoK.Name = "txtCtoK";
-            txtCtoK.Size = new Size(100, 23);
+            txtCtoK.Size = new Size(100, 24);
             txtCtoK.TabIndex = 1;
             // 
             // txtKtoC
             // 
+            txtKtoC.Font = new Font("Comic Sans MS", 9F);
             txtKtoC.Location = new Point(222, 109);
             txtKtoC.Name = "txtKtoC";
-            txtKtoC.Size = new Size(100, 23);
+            txtKtoC.Size = new Size(100, 24);
             txtKtoC.TabIndex = 2;
             // 
             // txtMetoPe
             // 
+            txtMetoPe.Font = new Font("Comic Sans MS", 9F);
             txtMetoPe.Location = new Point(12, 276);
             txtMetoPe.Name = "txtMetoPe";
-            txtMetoPe.Size = new Size(100, 23);
+            txtMetoPe.Size = new Size(100, 24);
             txtMetoPe.TabIndex = 1;
             // 
             // txtPetoMe
             // 
+            txtPetoMe.Font = new Font("Comic Sans MS", 9F);
             txtPetoMe.Location = new Point(221, 276);
             txtPetoMe.Name = "txtPetoMe";
-            txtPetoMe.Size = new Size(100, 23);
+            txtPetoMe.Size = new Size(100, 24);
             txtPetoMe.TabIndex = 2;
             // 
             // txtKmtoMi
             // 
+            txtKmtoMi.Font = new Font("Comic Sans MS", 9F);
             txtKmtoMi.Location = new Point(12, 329);
             txtKmtoMi.Name = "txtKmtoMi";
-            txtKmtoMi.Size = new Size(100, 23);
+            txtKmtoMi.Size = new Size(100, 24);
             txtKmtoMi.TabIndex = 1;
             // 
             // txtMitoKm
             // 
+            txtMitoKm.Font = new Font("Comic Sans MS", 9F);
             txtMitoKm.Location = new Point(221, 329);
             txtMitoKm.Name = "txtMitoKm";
-            txtMitoKm.Size = new Size(100, 23);
+            txtMitoKm.Size = new Size(100, 24);
             txtMitoKm.TabIndex = 2;
             // 
             // label1
             // 
             label1.AutoSize = true;
+            label1.Font = new Font("Comic Sans MS", 9F);
             label1.Location = new Point(121, 9);
             label1.Name = "label1";
-            label1.Size = new Size(73, 15);
+            label1.Size = new Size(78, 17);
             label1.TabIndex = 3;
             label1.Text = "Temperatura";
             // 
             // label2
             // 
             label2.AutoSize = true;
+            label2.Font = new Font("Comic Sans MS", 9F);
             label2.Location = new Point(55, 47);
             label2.Name = "label2";
-            label2.Size = new Size(20, 15);
+            label2.Size = new Size(21, 17);
             label2.TabIndex = 4;
             label2.Text = "Cº";
             // 
             // label3
             // 
             label3.AutoSize = true;
+            label3.Font = new Font("Comic Sans MS", 9F);
             label3.Location = new Point(266, 47);
             label3.Name = "label3";
-            label3.Size = new Size(18, 15);
+            label3.Size = new Size(21, 17);
             label3.TabIndex = 4;
             label3.Text = "Fº";
             // 
             // label4
             // 
             label4.AutoSize = true;
+            label4.Font = new Font("Comic Sans MS", 9F);
             label4.Location = new Point(55, 91);
             label4.Name = "label4";
-            label4.Size = new Size(20, 15);
+            label4.Size = new Size(21, 17);
             label4.TabIndex = 4;
             label4.Text = "Cº";
             // 
             // label5
             // 
             label5.AutoSize = true;
+            label5.Font = new Font("Comic Sans MS", 9F);
             label5.Location = new Point(266, 91);
             label5.Name = "label5";
-            label5.Size = new Size(19, 15);
+            label5.Size = new Size(21, 17);
             label5.TabIndex = 4;
             label5.Text = "Kº";
             // 
             // label6
             // 
             label6.AutoSize = true;
+            label6.Font = new Font("Comic Sans MS", 9F);
             label6.Location = new Point(37, 258);
             label6.Name = "label6";
-            label6.Size = new Size(44, 15);
+            label6.Size = new Size(48, 17);
             label6.TabIndex = 4;
             label6.Text = "Metros";
             // 
             // label7
             // 
             label7.AutoSize = true;
+            label7.Font = new Font("Comic Sans MS", 9F);
             label7.Location = new Point(259, 258);
             label7.Name = "label7";
-            label7.Size = new Size(25, 15);
+            label7.Size = new Size(26, 17);
             label7.TabIndex = 4;
             label7.Text = "Pés";
             // 
             // label8
             // 
             label8.AutoSize = true;
+            label8.Font = new Font("Comic Sans MS", 9F);
             label8.Location = new Point(27, 311);
             label8.Name = "label8";
-            label8.Size = new Size(64, 15);
+            label8.Size = new Size(67, 17);
             label8.TabIndex = 4;
             label8.Text = "Kilometros";
             // 
             // label9
             // 
             label9.AutoSize = true;
+            label9.Font = new Font("Comic Sans MS", 9F);
             label9.Location = new Point(247, 311);
             label9.Name = "label9";
-            label9.Size = new Size(42, 15);
+            label9.Size = new Size(44, 17);
             label9.TabIndex = 4;
             label9.Text = "Milhas";
             // 
             // label10
             // 
             label10.AutoSize = true;
+            label10.Font = new Font("Comic Sans MS", 9F);
             label10.Location = new Point(139, 233);
             label10.Name = "label10";
-            label10.Size = new Size(55, 15);
+            label10.Size = new Size(61, 17);
             label10.TabIndex = 3;
             label10.Text = "Distancia";
             // 
             // txtFtoK
             // 
+            txtFtoK.Font = new Font("Comic Sans MS", 9F);
             txtFtoK.Location = new Point(12, 154);
             txtFtoK.Name = "txtFtoK";
-            txtFtoK.Size = new Size(100, 23);
+            txtFtoK.Size = new Size(100, 24);
             txtFtoK.TabIndex = 1;
             // 
             // txtKtoF
             // 
+            txtKtoF.Font = new Font("Comic Sans MS", 9F);
             txtKtoF.Location = new Point(222, 154);
             txtKtoF.Name = "txtKtoF";
-            txtKtoF.Size = new Size(100, 23);
+            txtKtoF.Size = new Size(100, 24);
             txtKtoF.TabIndex = 2;
             // 
             // label12
             // 
             label12.AutoSize = true;
+            label12.Font = new Font("Comic Sans MS", 9F);
             label12.Location = new Point(55, 138);
             label12.Name = "label12";
-            label12.Size = new Size(18, 15);
+            label12.Size = new Size(21, 17);
             label12.TabIndex = 4;
             label12.Text = "Fº";
             // 
             // label13
             // 
             label13.AutoSize = true;
+            label13.Font = new Font("Comic Sans MS", 9F);
             label13.Location = new Point(266, 138);
             label13.Name = "label13";
-            label13.Size = new Size(19, 15);
+            label13.Size = new Size(21, 17);
             label13.TabIndex = 4;
             label13.Text = "Kº";
             // 
             // txtCmtoPo
             // 
+            txtCmtoPo.Font = new Font("Comic Sans MS", 9F);
             txtCmtoPo.Location = new Point(12, 382);
             txtCmtoPo.Name = "txtCmtoPo";
-            txtCmtoPo.Size = new Size(100, 23);
+            txtCmtoPo.Size = new Size(100, 24);
             txtCmtoPo.TabIndex = 1;
             // 
             // txtPotoCm
             // 
+            txtPotoCm.Font = new Font("Comic Sans MS", 9F);
             txtPotoCm.Location = new Point(221, 382);
             txtPotoCm.Name = "txtPotoCm";
-            txtPotoCm.Size = new Size(100, 23);
+            txtPotoCm.Size = new Size(100, 24);
             txtPotoCm.TabIndex = 2;
             // 
             // label11
             // 
             label11.AutoSize = true;
+            label11.Font = new Font("Comic Sans MS", 9F);
             label11.Location = new Point(27, 364);
             label11.Name = "label11";
-            label11.Size = new Size(72, 15);
+            label11.Size = new Size(75, 17);
             label11.TabIndex = 4;
             label11.Text = "Centimetros";
             // 
             // label14
             // 
             label14.AutoSize = true;
+            label14.Font = new Font("Comic Sans MS", 9F);
             label14.Location = new Point(237, 364);
             label14.Name = "label14";
-            label14.Size = new Size(61, 15);
+            label14.Size = new Size(60, 17);
             label14.TabIndex = 4;
             label14.Text = "Polegadas";
             // 
