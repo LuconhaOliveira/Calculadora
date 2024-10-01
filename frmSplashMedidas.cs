213ea79e1fa1f897af62eaeab6c7e472492288bc
LuconhaOliveira <lucaas.oliveira0254@gmail.com>
2024-10-01 10:36:28 -0300

    Feito o loading das medidas aaaaa

---
+++ frmSplashMedidas.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class frmSplashMedidas : Form
+    {
+        public frmSplashMedidas()
+        {
+            InitializeComponent();
+        }
+
+        private void tmrLoading_Tick(object sender, EventArgs e)
+        {
+            if (pgbLoading.Value >= 7)
+            {
+                tmrLoading.Enabled = false;
+                this.Hide();
+                new frmMedidas().ShowDialog();
+            }
+            else
+            {
+                pgbLoading.Value++;
+            }
+        }
+    }
+}
